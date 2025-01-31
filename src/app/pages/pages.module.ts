a41be58073ea5c491b8701c51e03e83fd32ff145
Alvaro Torres <alvarojavier.torres@dxc.com>
2025-01-31 10:41:07 -0500

    feat(): pruebas con nebular

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NbButtonModule, NbLayoutModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { SharedModule } from '../shared/shared.module';
 import { PagesComponent } from './pages.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { RouterModule } from '@angular/router';
@@ -20,7 +18,6 @@ import { HomeComponent } from './home/home.component';
     // NbSidebarModule,
     // NbButtonModule,
     // NbEvaIconsModule,
-    SharedModule,
     AppRoutingModule,
     RouterModule,
 
